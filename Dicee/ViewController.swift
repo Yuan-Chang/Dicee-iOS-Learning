79cf1c88634392a134c278a7b1f58f2db892536c
Yuan Chang <yuanchang_chang@intuit.com>
2019-08-06 14:15:36 -0700

    first commit

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var randomDice1: UIImageView!
+    @IBOutlet weak var randomDice2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateDiceImages()
     }
-
-
+    
+    @IBAction func RollBtnClicked(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDice1.image = UIImage(named: "dice\(Int.random(in: 1 ... 5))");
+        randomDice2.image = UIImage(named: "dice\(Int.random(in: 1 ... 5))");
+    }
+    
 }
 
